334c16528157cb746732384a6b5226555d7ccde1
vq <1457480465@qq.com>
2023-11-18 22:54:05 +0800

    Atcoder abc329 A-D

---
+++ atcoder/src/com/fqh/abc/abc_329/B.java
@@ -0,0 +1,97 @@
+package com.fqh.abc.abc_329;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * @Author: vq
+ * @Date: 2023/11/18 20:09
+ * @Version V1.0
+ */
+public class B {
+
+    static final int MAXN = 200010;
+    static int[] a = new int[MAXN];
+    static int[] b = new int[MAXN];
+    static boolean[] vis = new boolean[MAXN];
+    static List<Integer>[] g;
+    static int n;
+    static int m;
+
+    static void solve() throws Exception {
+        n = in.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; ++i) a[i] = in.nextInt();
+        Arrays.sort(a);
+        int last = a[n - 1];
+        int i = n - 1;
+        while (last == a[i]) {
+            i--;
+        }
+        out.println(a[i]);
+    }
+
+    public static void main(String[] args) throws Exception {
+        int T = 1;
+        while (T-- > 0) {
+            solve();
+        }
+        out.close();
+    }
+
+    static class Pair implements Comparable<Pair> {
+        int x;
+        int y;
+
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            if (this.x == o.x) {
+                return this.y - o.y;
+            }
+            return this.x - o.x;
+        }
+    }
+
+    static InputReader in = new InputReader();
+    static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+    static class InputReader {
+        private StringTokenizer st;
+        private BufferedReader bf;
+
+        public InputReader() {
+            bf = new BufferedReader(new InputStreamReader(System.in));
+            st = null;
+        }
+
+        public String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(bf.readLine());
+            }
+            return st.nextToken();
+        }
+
+        public String nextLine() throws IOException {
+            return bf.readLine();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+    }
+}
