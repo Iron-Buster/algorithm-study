a19a8b37425d6a89394b7a2dbf3b91270fe0feb8
vq <1457480465@qq.com>
2023-12-04 22:32:21 +0800

    树状数组模板

---
+++ algorithm-constent-template/src/com/fqh/FenwickTreeTemplate.java
@@ -0,0 +1,23 @@
+package com.fqh;
+
+/**
+ * @Author: vq
+ * @Date: 2023/11/24 12:10
+ * @Version V1.0
+ */
+
+/**
+ * 树状数组
+ */
+public class FenwickTreeTemplate {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
+class FenwickTree {
+
+}
