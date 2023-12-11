57ce4fe00f1fd90de5688c3451d0a0caacc2c8e2
vq <1457480465@qq.com>
2023-12-11 22:02:39 +0800

    TreeNode类

---
+++ leetcode/src/com/fqh/utils/TreeNode.java
@@ -0,0 +1,26 @@
+package com.fqh.utils;
+
+/**
+ * @Author: vq
+ * @Date: 2023/12/4 11:51
+ * @Version V1.0
+ */
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
