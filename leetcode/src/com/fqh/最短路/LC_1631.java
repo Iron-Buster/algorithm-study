61a665fe9271b8dcde8e5d149bbb844b7f5937f4
vq <1457480465@qq.com>
2023-12-11 21:58:48 +0800

    最短路-dijkstra

---
+++ leetcode/src/com/fqh/最短路/LC_1631.java
@@ -0,0 +1,64 @@
+package com.fqh.最短路;
+
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+/**
+ * @Author: vq
+ * @Date: 2023/12/11 10:41
+ * @Version V1.0
+ */
+public class LC_1631 {
+//    1631. 最小体力消耗路径
+//    第 212 场周赛
+//            Q3
+//    1948
+//    相关标签
+//            相关企业
+//    提示
+//    你准备参加一场远足活动。给你一个二维 rows x columns 的地图 heights ，其中 heights[row][col] 表示格子 (row, col) 的高度。
+//    一开始你在最左上角的格子 (0, 0) ，且你希望去最右下角的格子 (rows-1, columns-1) （注意下标从 0 开始编号）。
+//    你每次可以往 上，下，左，右 四个方向之一移动，你想要找到耗费 体力 最小的一条路径。
+//
+//    一条路径耗费的 体力值 是路径上相邻格子之间 高度差绝对值 的 最大值 决定的。
+//
+//    请你返回从左上角走到右下角的最小 体力消耗值 。
+
+    boolean[][] vis;
+    static final int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+//    Dijkstra-最短路
+    public int minimumEffortPath(int[][] heights) {
+        int m = heights.length;
+        int n = heights[0].length;
+        vis = new boolean[m][n];
+        var pq = new PriorityQueue<int[]>((o1, o2) -> o1[2] - o2[2]);
+        pq.offer(new int[]{0, 0, 0});
+        while (pq.size() > 0) {
+            var p = pq.poll();
+            int x = p[0], y = p[1], mx = p[2];
+            if (vis[x][y]) continue;
+            vis[x][y] = true;
+            if (x == m - 1 && y == n - 1) {
+                return mx;
+            }
+            for (int[] dir : dirs) {
+                int nx = x + dir[0], ny = y + dir[1];
+                if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+                pq.offer(new int[]{nx, ny, Math.max(mx, Math.abs(heights[x][y] - heights[nx][ny]))});
+            }
+        }
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        int[][] heights = {
+                {1,2,2},
+                {3,8,2},
+                {5,3,5}
+        };
+        System.out.println(new LC_1631().minimumEffortPath(heights));
+    }
+}
---
+++ leetcode/src/com/fqh/最短路/LC_743.java
@@ -0,0 +1,82 @@
+package com.fqh.最短路;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * @Author: vq
+ * @Date: 2023/12/11 21:50
+ * @Version V1.0
+ */
+public class LC_743 {
+
+//    743. 网络延迟时间
+//            中等
+//    相关标签
+//            相关企业
+//    提示
+//    有 n 个网络节点，标记为 1 到 n。
+//
+//    给你一个列表 times，表示信号经过 有向 边的传递时间。 times[i] = (ui, vi, wi)，其中 ui 是源节点，vi 是目标节点， wi 是一个信号从源节点传递到目标节点的时间。
+//
+//    现在，从某个节点 K 发出一个信号。需要多久才能使所有节点都收到信号？如果不能使所有节点收到信号，返回 -1 。
+
+    public int networkDelayTime(int[][] times, int n, int k) {
+        g = new ArrayList[n + 1];
+        dist = new long[n + 1];
+        vis = new boolean[n + 1];
+        for (int i = 1; i <= n; ++i) {
+            g[i] = new ArrayList<>();
+            dist[i] = 0x3f3f3f;
+        }
+        for (int[] t : times) {
+            int u = t[0], v = t[1];
+            long w = t[2];
+            g[u].add(new Edge(v, w));
+        }
+        dijkstra(k, n);
+        long ans = Arrays.stream(dist).max().getAsLong();
+        return ans == 0x3f3f3f ? -1 : (int) ans;
+    }
+
+    long[] dist;
+    boolean[] vis;
+    List<Edge>[] g;
+
+    class Edge {
+        int v;
+        long w;
+
+        Edge(int v, long w) {
+            this.v = v;
+            this.w = w;
+        }
+    }
+
+    boolean dijkstra(int start, int end) {
+        var pq = new PriorityQueue<Edge>((a, b) -> Long.compare(a.w, b.w));
+        pq.offer(new Edge(start, 0));
+        dist[start] = 0;
+        while (!pq.isEmpty()) {
+            var p = pq.poll();
+            int u = p.v;
+            vis[u] = true;
+            for (Edge next : g[u]) {
+                int v = next.v;
+                long w = next.w;
+                if (!vis[v] && dist[u] + w < dist[v]) {
+                    dist[v] = dist[u] + w;
+                    pq.offer(new Edge(v, dist[v]));
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] times = {{1, 2, 1}};
+        System.out.println(new LC_743().networkDelayTime(times, 2, 2));
+    }
+}
