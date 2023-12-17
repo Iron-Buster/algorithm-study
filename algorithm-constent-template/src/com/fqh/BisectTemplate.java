723065e60318fbdba679b9ee11a3be1656cd1df3
vq <1457480465@qq.com>
2023-12-17 16:12:34 +0800

    模板更新

--- algorithm-constent-template/src/com/fqh/BisectTemplate.java
+++ algorithm-constent-template/src/com/fqh/BisectTemplate.java
@@ -6,6 +6,8 @@ package com.fqh;
  * @Version V1.0
  */
 
+import java.util.List;
+
 /**
  * 二分搜索模板
  */
@@ -18,7 +20,7 @@ public class BisectTemplate {
      * @param target    搜索的值
      * @return
      */
-    public static int bisectLeft(int[] a, int target) {
+    public static int bisectLeft(int[] a, long target) {
         int left = 0, right = a.length - 1;
         while (left <= right) {
             int mid = (left + right) >> 1;
@@ -38,7 +40,7 @@ public class BisectTemplate {
      * @param target    搜索的值
      * @return
      */
-    public static int bisectRight(int[] a, int target) {
+    public static int bisectRight(int[] a, long target) {
         int left = 0, right = a.length - 1;
         while (left <= right) {
             int mid = (left + right) >> 1;
@@ -59,7 +61,7 @@ public class BisectTemplate {
      * @param key       搜索的key
      * @return
      */
-    public static int bisectLeft1(int[][] a, int target, int key) {
+    public static int bisectLeft1(int[][] a, long target, int key) {
         int left = 0, right = a.length - 1;
         while (left <= right) {
             int mid = (left + right) >> 1;
@@ -81,11 +83,52 @@ public class BisectTemplate {
      * @param key       搜索的key
      * @return
      */
-    public static int bisectRight1(int[][] a, int target, int key) {
+    public static int bisectRight1(int[][] a, long target, int key) {
         int left = 0, right = a.length - 1;
         while (left <= right) {
             int mid = (left + right) >> 1;
             if (a[mid][key] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+
+    /**
+     * 返回 `target` 在 `a` 中最左边的插入位置。
+     * 存在多个相同的值时，返回最左边的位置。
+     * @param a         list集合
+     * @param target    搜索的值
+     * @return
+     */
+    public static int bisectLeft2(List<Integer> a, long target) {
+        int left = 0, right = a.size() - 1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (a.get(mid) < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    /**
+     * 返回 `target` 在 `a` 中最右边的插入位置。
+     * 存在多个相同的值时，返回最右边的位置。
+     * @param a         list集合
+     * @param target    搜索的值
+     * @return
+     */
+    public static int bisectRight2(List<Integer> a, long target) {
+        int left = 0, right = a.size() - 1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (a.get(mid) <= target) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
