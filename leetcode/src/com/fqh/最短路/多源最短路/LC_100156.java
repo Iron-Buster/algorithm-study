405bdbb829b17e5a27a71d0bb9e5d6d3f8caeb5c
vq <1457480465@qq.com>
2023-12-24 19:30:34 +0800

    floyd算法

---
+++ leetcode/src/com/fqh/最短路/多源最短路/LC_100156.java
@@ -0,0 +1,62 @@
+package com.fqh.最短路.多源最短路;
+
+import java.util.Arrays;
+
+/**
+ * @Author: vq
+ * @Date: 2023/12/24 19:18
+ * @Version V1.0
+ */
+public class LC_100156 {
+
+    public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
+        long[][] dist = new long[26][26];
+        for (int i = 0; i < 26; i++) {
+            Arrays.fill(dist[i], Long.MAX_VALUE / 2);
+            dist[i][i] = 0;
+        }
+        for (int i = 0; i < original.length; i++) {
+            int from = original[i] - 'a';
+            int to = changed[i] - 'a';
+            dist[from][to] = Math.min(dist[from][to], cost[i]);
+        }
+        floyd(dist);
+        long ans = 0;
+        for (int i = 0; i < source.length(); i++) {
+            int from = source.charAt(i) - 'a';
+            int to = target.charAt(i) - 'a';
+            if (dist[from][to] == Long.MAX_VALUE / 2) {
+                return -1;
+            }
+            ans += dist[from][to];
+        }
+        return ans;
+    }
+
+    public static long[][] floyd(long[][] dist) {
+        int n = dist.length;
+        for (int k = 0; k < n; ++k) {
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    // 不选 k，选 k
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+        return dist;
+    }
+
+
+//    100156. 转换字符串的最小成本 I
+//    尝试过
+//            中等
+//    相关企业
+//            提示
+//    给你两个下标从 0 开始的字符串 source 和 target ，它们的长度均为 n 并且由 小写 英文字母组成。
+//
+//    另给你两个下标从 0 开始的字符数组 original 和 changed ，以及一个整数数组 cost ，其中 cost[i] 代表将字符 original[i] 更改为字符 changed[i] 的成本。
+//
+//    你从字符串 source 开始。在一次操作中，如果 存在 任意 下标 j 满足 cost[j] == z  、original[j] == x 以及 changed[j] == y 。你就可以选择字符串中的一个字符 x 并以 z 的成本将其更改为字符 y 。
+//
+//    返回将字符串 source 转换为字符串 target 所需的 最小 成本。如果不可能完成转换，则返回 -1 。
+}
