28fd046ec93f2ee4ca5d5ef6e79f488176e8202f
vq <1457480465@qq.com>
2024-01-09 00:16:22 +0800

    线段树维护区间最大字段和模板

--- algorithm-constent-template/src/com/fqh/SegmentTreeTemplate.java
+++ algorithm-constent-template/src/com/fqh/SegmentTreeTemplate.java
@@ -191,3 +191,160 @@ class LG_3372 {
 }
 
 
+// https://www.luogu.com.cn/problem/SP1716
+//https://www.spoj.com/problems/GSS3/
+class SP_1716 {
+
+    public static void solve() throws IOException {
+        int n = in.nextInt();
+        SegmentTreeIntervalMaxSubSum st = new SegmentTreeIntervalMaxSubSum();
+        for (int i = 1; i <= n; i++) {
+            st.a[i] = in.nextInt();
+        }
+        int m = in.nextInt();
+        st.build(1, 1, n);
+        while (m-- > 0) {
+            String s = in.nextLine();
+            String[] ss = s.split(" ");
+            int k = Integer.parseInt(ss[0]);
+            int x = Integer.parseInt(ss[1]);
+            int y = Integer.parseInt(ss[2]);
+            if (k == 1) {
+                if (x > y) {
+                    int t = x;
+                    x = y;
+                    y = t;
+                }
+                int ans = st.query(1, x, y).mx;
+                out.println(ans);
+            } else {
+                st.change(1, x, y);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int T = 1;
+        while (T-- > 0) {
+            solve();
+        }
+        out.close();
+    }
+
+    static InputReader in = new InputReader();
+    static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+    static class InputReader {
+        private StringTokenizer st;
+        private BufferedReader bf;
+
+        public InputReader() {
+            bf = new BufferedReader(new InputStreamReader(System.in));
+            st = null;
+        }
+
+        public String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(bf.readLine());
+            }
+            return st.nextToken();
+        }
+
+        public String nextLine() throws IOException {
+            return bf.readLine();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+    }
+}
+
+// 线段树 区间最大字段和
+class SegmentTreeIntervalMaxSubSum {
+
+    static final int N = 50005;
+    int[] a = new int[N];
+    Tree[] tree = new Tree[4 * N];
+
+    void pushUp(Tree u, Tree l, Tree r) {
+        u.sum = l.sum + r.sum;
+        u.lmx = Math.max(l.lmx, l.sum + r.lmx);
+        u.rmx = Math.max(r.rmx, r.sum + l.rmx);
+        u.mx = Math.max(Math.max(l.mx, r.mx), l.rmx + r.lmx);
+    }
+
+    void build(int u, int l, int r) { // 建树
+        tree[u] = new Tree(l, r, a[r], a[r], a[r], a[r]);
+        if (l == r) return;
+        int m = l + r >> 1;
+        int ls = u << 1;
+        int rs = u << 1 | 1;
+        build(ls, l, m);
+        build(rs, m + 1, r);
+        pushUp(tree[u], tree[ls], tree[rs]);
+    }
+
+
+    void change(int u, int x, int v) { // 点修
+        if (tree[u].l == tree[u].r) {
+            tree[u] = new Tree(x, x, v, v, v, v);
+            return;
+        }
+        int mid = tree[u].l + tree[u].r >> 1;
+        int ls = u << 1;
+        int rs = u << 1 | 1;
+        if (x <= mid) {
+            change(ls, x, v);
+        } else {
+            change(rs, x, v);
+        }
+        pushUp(tree[u], tree[ls], tree[rs]);
+    }
+
+    Tree query(int u, int l, int r) { // 区查
+        if (l <= tree[u].l &&  tree[u].r <= r) {
+            return tree[u];
+        }
+        int mid = tree[u].l + tree[u].r >> 1;
+        int ls = u << 1;
+        int rs = u << 1 | 1;
+        if (r <= mid) {
+            return query(ls, l, r);
+        }
+        if (l > mid) {
+            return query(rs, l, r);
+        }
+        // 开一个临时节点，存储拼凑结果（动态开点线段树）
+        Tree T = new Tree();
+        pushUp(T, query(ls, l, mid), query(rs, mid + 1, r));
+        return T;
+    }
+
+    static class Tree { // 线段树
+        int l, r;
+        // 区间和，最大左子段和，最大右子段和，最大子段和
+        int sum, lmx, rmx, mx;
+
+        public Tree(int l, int r, int sum, int lmx, int rmx, int mx) {
+            this.l = l;
+            this.r = r;
+            this.sum = sum;
+            this.lmx = lmx;
+            this.rmx = rmx;
+            this.mx = mx;
+        }
+
+        public Tree() {}
+    }
+}
+
+
+
