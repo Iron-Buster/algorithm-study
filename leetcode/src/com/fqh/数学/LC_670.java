9aef021dcf7e03d088eceaa8536a4185d19e9259
vq <1457480465@qq.com>
2024-01-22 10:36:54 +0800

    力扣每日一题🥲

--- leetcode/src/com/fqh/数学/LC_670.java
+++ leetcode/src/com/fqh/数学/LC_670.java
@@ -7,6 +7,27 @@ package com.fqh.数学;
  **/
 public class LC_670 {
 
+//    670. 最大交换 https://leetcode.cn/problems/maximum-swap/description/?envType=daily-question&envId=2024-01-22
+//            已解答
+//    中等
+//            相关标签
+//    相关企业
+//    给定一个非负整数，你至多可以交换一次数字中的任意两位。返回你能得到的最大值。
+//
+//    示例 1 :
+//
+//    输入: 2736
+//    输出: 7236
+//    解释: 交换数字2和数字7。
+//    示例 2 :
+//
+//    输入: 9973
+//    输出: 9973
+//    解释: 不需要交换。
+//    注意:
+//
+//    给定数字的范围是 [0, 108]
+
     public int maximumSwap(int num) {
         return f(num);
     }
