d6d31adc5d77f1f91f0e1c74d29084c7a137caff
vq <1457480465@qq.com>
2024-01-24 18:09:00 +0800

    🚀

---
+++ go-code/dp/partition-equal-subset-sum.go
@@ -0,0 +1,49 @@
+package main
+
+/**
+416. 分割等和子集
+已解答
+中等
+相关标签
+相关企业
+给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+
+
+示例 1：
+输入：nums = [1,5,11,5]
+输出：true
+解释：数组可以分割成 [1, 5, 5] 和 [11] 。
+示例 2：
+输入：nums = [1,2,3,5]
+输出：false
+解释：数组不能分割成两个元素和相等的子集。
+*/
+
+func canPartition(nums []int) bool {
+	//n := len(nums)
+	//sum := 0
+	//for _, x := range nums {
+	//	sum += x
+	//}
+	//if sum%2 != 0 {
+	//	return false
+	//}
+	//t := sum / 2
+	//f := make([][]bool, n)
+	//for i := range f {
+	//	f[i] = make([]bool, t)
+	//}
+	//var dfs func(i, v int) int
+	//dfs = func(i, v int) bool {
+	//	if i >= n {
+	//		return v == 0
+	//	}
+	//	if f[i][v] != nil {
+	//	}
+	//}
+	return true
+}
+
+func main() {
+
+}
