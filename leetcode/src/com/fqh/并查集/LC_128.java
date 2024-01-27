c46bf825676c6c7effc8f17e52ea45595261dc7c
vq <1457480465@qq.com>
2024-01-27 17:53:09 +0800

    并查集

--- leetcode/src/com/fqh/并查集/LC_128.java
+++ leetcode/src/com/fqh/并查集/LC_128.java
@@ -9,6 +9,15 @@ import java.util.HashMap;
  * @since 2024/1/27 17:45
  **/
 public class LC_128 {
+
+//    128. 最长连续序列
+//            已解答
+//    中等
+//            相关标签
+//    相关企业
+//    给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+//
+//    请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
     public int longestConsecutive(int[] nums) {
         int n = nums.length;
         UnionFind uf = new UnionFind(n);
