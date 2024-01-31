9c496dbf205ff93a31df974992d3200f9f1fe292
vq <1457480465@qq.com>
2024-01-31 17:43:44 +0800

    make nums sorted min ops

---
+++ py-code/技巧/2086.从房屋收集雨水需要的最少水桶数.py
@@ -0,0 +1,61 @@
+from itertools import *
+from collections import *
+from math import *
+from cmath import *
+from heapq import *
+from functools import *
+from bisect import *
+from random import *
+from typing import *
+import random
+import sys
+import os
+from io import BytesIO, IOBase
+from copy import deepcopy
+import threading
+
+BUFSIZE = 4096
+MOD = 10 ** 9 + 7
+mod = 998244353
+inf = float('inf')
+def PF(a):
+    return [0] + list(accumulate(a))
+
+'''
+2086. 从房屋收集雨水需要的最少水桶数
+第 66 场双周赛
+Q2
+1623
+相关标签
+相关企业
+提示
+给你一个下标从 0 开始的字符串 street 。street 中每个字符要么是表示房屋的 'H' ，要么是表示空位的 '.' 。
+
+你可以在 空位 放置水桶，从相邻的房屋收集雨水。位置在 i - 1 或者 i + 1 的水桶可以收集位置为 i 处房屋的雨水。一个水桶如果相邻两个位置都有房屋，那么它可以收集 两个 房屋的雨水。
+
+在确保 每个 房屋旁边都 至少 有一个水桶的前提下，请你返回需要的 最少 水桶数。如果无解请返回 -1 。
+
+ 
+
+示例 1：
+
+输入：street = "H..H"
+输出：2
+解释：
+我们可以在下标为 1 和 2 处放水桶。
+"H..H" -> "HBBH"（'B' 表示放置水桶）。
+下标为 0 处的房屋右边有水桶，下标为 3 处的房屋左边有水桶。
+所以每个房屋旁边都至少有一个水桶收集雨水。
+'''
+
+class Solution:
+    def minimumBuckets(self, h: str) -> int:
+        if h.count('.') == 0: return -1
+        if h.count('H') == 0: return 0
+        h = h.replace('H.H', '1')
+        h = h.replace('.H', '1')
+        h = h.replace('H.', '1')
+        return -1 if 'H' in h else h.count('1')
+
+if __name__ == '__main__':
+    print(Solution().minimumBuckets('.......HH'))
