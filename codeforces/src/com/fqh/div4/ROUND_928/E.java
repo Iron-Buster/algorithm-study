4e2562d9788def9c99fd535d1f21f9dfdf062eed
vq <1457480465@qq.com>
2024-02-22 15:45:15 +0800

    codeforces div4 round928 E

--- codeforces/src/com/fqh/div4/ROUND_928/E.java
+++ codeforces/src/com/fqh/div4/ROUND_928/E.java
@@ -19,39 +19,6 @@ public class E {
     public static void solve() throws IOException {
         long n = in.nextLong();
         long k = in.nextLong();
-        var a = new ArrayList<Long>();
-        var vis = new HashSet<Long>();
-        for (long i = 1; i <= n; i++) {
-            if (i % 2 == 1) {
-                a.add(i);
-                vis.add(i);
-            }
-        }
-        List<Long> b = new ArrayList<>();
-        for (long x : a) {
-            if (x * 2 > n) break;
-            if (vis.contains(x * 2)) continue;
-            b.add(x * 2);
-            vis.add(x * 2);
-        }
-        List<Long> c = new ArrayList<>();
-        for (long x : a) {
-            if (x * 3 > n) break;
-            if (vis.contains(x * 3)) continue;
-            c.add(x * 3);
-            vis.add(x * 3);
-        }
-        List<Long> d = new ArrayList<>();
-        for (long x : a) {
-            if (x * 4 > n) break;
-            if (vis.contains(x * 4)) continue;
-            d.add(x * 4);
-            vis.add(x * 4);
-        }
-        a.addAll(b);
-        a.addAll(c);
-        a.addAll(d);
-        out.println(a.get((int) (k - 1)));
     }
 
     static boolean MULTI_CASE = true;
