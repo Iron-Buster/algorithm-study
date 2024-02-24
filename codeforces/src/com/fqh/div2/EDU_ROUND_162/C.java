4dedfb6fef574fb7753ad2b1382e36d888a2dc02
vq <1457480465@qq.com>
2024-02-24 12:23:37 +0800

    CF edu div2 A-C

--- codeforces/src/com/fqh/div2/EDU_ROUND_162/C.java
+++ codeforces/src/com/fqh/div2/EDU_ROUND_162/C.java
@@ -11,35 +11,34 @@ import java.util.StringTokenizer;
 public class C {
 
 
+    // 1比较特殊，它不能减1
+    // 区间的增量如果大于了区间1的个数，说明我们可以让每个1加1，然后较大的那个数一直减
+    // 等价于判断区间内大于1的部分是否能让所有至少加上1
 
     public static void solve() throws IOException {
         int n = in.nextInt();
         int q = in.nextInt();
         long[] c = new long[n];
         long[] s = new long[n+1];
+        long[] cnt1 = new long[n+1]; // 区间1的数
         for (int i = 0; i < n; i++) {
             c[i] = in.nextLong();
             s[i+1] = s[i] + c[i];
+            cnt1[i+1] = cnt1[i] + (c[i] == 1 ? 1 : 0);
         }
         while (q-- > 0) {
-            int l = in.nextInt() - 1;
-            int r = in.nextInt() - 1;
+            int l = in.nextInt();
+            int r = in.nextInt();
             if (l == r) {
                 out.println("NO");
                 continue;
             }
-            long d = s[r+1] - s[l];
-            int cnt = r - l + 1;
-            if (d == cnt) {
-                out.println("NO");
-                continue;
-            }
-            if (d >= cnt * 2L) {
+            long t = s[r] - s[l-1] - (r - l + 1); // 区间[L,R]的增量
+            if (t >= cnt1[r] - cnt1[l-1]) {
                 out.println("YES");
             } else {
                 out.println("NO");
             }
-
         }
     }
 
