61948b24bebce2eed2de5d21773affa4e14088f8
vq <1457480465@qq.com>
2024-03-06 16:46:40 +0800

    🌻 KMP算法模板

--- algorithm-constent-template/src/com/fqh/StringTemplate.java
+++ algorithm-constent-template/src/com/fqh/StringTemplate.java
@@ -6,11 +6,25 @@ package com.fqh;
  * @since 2024/3/6 16:26
  **/
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 字符串算法相关模板
  */
 public class StringTemplate {
 
+    public static void main(String[] args) {
+        String a = "ababab";
+        String b = "ab";
+        KMP kmp = new KMP();
+        int count = kmp.matchCount(a, b);
+        System.out.println(count);
+        int firstIndex = kmp.matchFirstIndex(a, b);
+        System.out.println(firstIndex);
+        List<Integer> matchAllIndex = kmp.matchAllIndex(a, b);
+        System.out.println(matchAllIndex);
+    }
 }
 
 /**
@@ -60,5 +74,110 @@ class StringHash {
         for (int i = 0; i < n; i++) {
             s[i+1] = str.charAt(i);
         }
+    }
+}
+
+/**
+ * KMP算法
+ */
+class KMP {
+
+    char[] s;
+    char[] p;
+    int n, m;
+    int[] next;
+
+    public KMP() {}
+
+    /**
+     * 返回 b在a中匹配的第一次出现的位置
+     */
+    int matchFirstIndex(String a, String b) {
+        this.n = a.length();
+        this.m = b.length();
+        a = " " + a;
+        b = " " + b;
+
+        this.s = a.toCharArray();
+        this.p = b.toCharArray();
+        this.next = new int[m+1];
+
+        int[] next = new int[m + 1];
+        for (int i = 2, j = 0; i <= m; i++) {
+            while (j > 0 && p[i] != p[j + 1]) j = next[j];
+            if (p[i] == p[j + 1]) j++;
+            next[i] = j;
+        }
+        for (int i = 1, j = 0; i <= n; i++) {
+            while (j > 0 && s[i] != p[j + 1]) j = next[j];
+            if (s[i] == p[j + 1]) j++;
+            if (j == m) {
+                j = next[j];
+                return i - m;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 返回 b在a中匹配的所有位置
+     * @return 如果这些下标会被用来查找，则返回TreeSet。
+     */
+    List<Integer> matchAllIndex(String a, String b) {
+        this.n = a.length();
+        this.m = b.length();
+        a = " " + a;
+        b = " " + b;
+
+        this.s = a.toCharArray();
+        this.p = b.toCharArray();
+        this.next = new int[m+1];
+
+        List<Integer> list = new ArrayList<>();
+        int[] next = new int[m + 1];
+        for (int i = 2, j = 0; i <= m; i++) {
+            while (j > 0 && p[i] != p[j + 1]) j = next[j];
+            if (p[i] == p[j + 1]) j++;
+            next[i] = j;
+        }
+        for (int i = 1, j = 0; i <= n; i++) {
+            while (j > 0 && s[i] != p[j + 1]) j = next[j];
+            if (s[i] == p[j + 1]) j++;
+            if (j == m) {
+                j = next[j];
+                list.add(i - m);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 返回b在a中匹配的次数
+     */
+    int matchCount(String a, String b) {
+        this.n = a.length();
+        this.m = b.length();
+        a = " " + a;
+        b = " " + b;
+        int ans = 0;
+        this.s = a.toCharArray();
+        this.p = b.toCharArray();
+        this.next = new int[m+1];
+
+        int[] next = new int[m + 1];
+        for (int i = 2, j = 0; i <= m; i++) {
+            while (j > 0 && p[i] != p[j + 1]) j = next[j];
+            if (p[i] == p[j + 1]) j++;
+            next[i] = j;
+        }
+        for (int i = 1, j = 0; i <= n; i++) {
+            while (j > 0 && s[i] != p[j + 1]) j = next[j];
+            if (s[i] == p[j + 1]) j++;
+            if (j == m) {
+                j = next[j];
+                ans++;
+            }
+        }
+        return ans;
     }
 }
