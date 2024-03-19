6bab04de1b5a60d7668dcadd6bba0fe4c76577c4
vq <1457480465@qq.com>
2024-03-19 18:44:00 +0800

    🥲lc-160 相交链表 双指针

---
+++ py-code/简单题/链表/LC_160.py
@@ -0,0 +1,44 @@
+from itertools import *
+from collections import *
+from math import *
+from cmath import *
+from heapq import *
+from functools import *
+from bisect import *
+from random import *
+from typing import *
+import random
+import sys
+import os
+from io import BytesIO, IOBase
+from copy import deepcopy
+import threading
+
+BUFSIZE = 4096
+MOD = 10 ** 9 + 7
+mod = 998244353
+inf = float('inf')
+def PF(a):
+    return [0] + list(accumulate(a))
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        a = headA
+        b = headB
+        while a != b:
+            if a:
+                a = a.next
+            else:
+                a = headB
+            if b:
+                b = b.next
+            else:
+                b = headA
+        return a
