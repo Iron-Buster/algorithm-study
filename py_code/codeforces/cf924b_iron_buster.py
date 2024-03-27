6ab0c464c0a512e9801906ccb3ee70dae467de75
vq <1457480465@qq.com>
2024-03-27 10:54:10 +0800

    😮 cf924b 双指针

---
+++ py_code/codeforces/cf924b_iron_buster.py
@@ -0,0 +1,121 @@
+from itertools import *
+from collections import *
+from math import *
+from cmath import *
+from heapq import *
+from functools import *
+from bisect import *
+import sys
+import os
+from io import BytesIO, IOBase
+
+BUFSIZE = 4096
+MOD = 10 ** 9 + 7
+mod = 998244353
+inf = float('inf')
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+# input = lambda: sys.stdin.readline().rstrip("\r\n")
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MI():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+
+def GMI():
+    return map(lambda x: int(x) - 1, input().split())
+
+
+def LGMI():
+    return list(map(lambda x: int(x) - 1, input().split()))
+
+
+def PF(a):
+    return [0] + list(accumulate(a))
+
+
+def solve():
+    n, u = MI()
+    a = LII()
+    i, k = 0, 2
+    ans = -1
+    # (a[k]-a[j]) / (a[k]-a[i])
+    # k越大越好, 枚举i, 更新答案
+    while i < n and k < n:
+        while k+1 < n and a[k+1] - a[i] <= u:
+            k += 1
+        if i < k-1 and a[k] - a[i] <= u:
+            ans = max(ans, (a[k] - a[i+1]) / (a[k] - a[i]))
+        i += 1
+    print(ans)
+
+
+def main():
+    t = 1
+    for _ in range(t):
+        solve()
+
+
+main()
