74c36e7ef5bb54b1280e96e74ce18ef5ea26b10f
vq <1457480465@qq.com>
2024-04-17 23:15:42 +0800

    🍔 倒序双指针 O(M + N)

--- py_code/技巧/双指针/LC_88.py
+++ py_code/技巧/双指针/LC_88.py
@@ -8,7 +8,24 @@ from typing import List
 
 
 class Solution:
+    # 倒序双指针 O(M + N)
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+
+    def merge2(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
