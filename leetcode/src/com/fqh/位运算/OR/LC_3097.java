772a08f65680a322724bbbd0cff043847d0056d6
vq <1457480465@qq.com>
2024-04-20 00:06:48 +0800

    🍔做法2：通用位运算模板

--- leetcode/src/com/fqh/位运算/OR/LC_3097.java
+++ leetcode/src/com/fqh/位运算/OR/LC_3097.java
@@ -1,5 +1,7 @@
 package com.fqh.位运算.OR;
 
+import java.util.ArrayList;
+
 /**
  * @author ikun
  * @version v1.0.0
@@ -9,7 +11,7 @@ public class LC_3097 {
 
 //    https://leetcode.cn/problems/shortest-subarray-with-or-at-least-k-ii/description/
 
-    public int minimumSubarrayLength(int[] nums, int k) {
+    public int minimumSubarrayLength2(int[] nums, int k) {
         int n = nums.length;
         int ans = Integer.MAX_VALUE;
         for (int i = 0; i < n; i++) {
@@ -26,5 +28,35 @@ public class LC_3097 {
             }
         }
         return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    public int minimumSubarrayLength(int[] nums, int k) {
+        int res = f(nums, k);
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
+
+
+    public static int f(int[] nums, int V) {
+        int n = nums.length;
+        int ans = Integer.MAX_VALUE;
+        ArrayList<int[]> ors = new ArrayList<>(); // 按位或的值 + 对应子数组的右端点的最小值
+        for (int i = n - 1; i >= 0; i--) {
+            ors.add(new int[]{0, i});
+            if (nums[i] >= V) return 1;
+            int k = 0;
+            for (int[] or : ors) {
+                or[0] |= nums[i];
+                if (or[0] >= V) ans = Math.min(ans, or[1] - i + 1);  // 最短
+                if (ors.get(k)[0] == or[0]) {
+                    ors.get(k)[1] = or[1];  // 合并相同值，下标取最小的
+                } else {
+                    ors.set(++k, or);
+                }
+            }
+            ors.subList(k + 1, ors.size()).clear();
+            // 本题只用到了 ors[0]，如果题目改成任意给定数值，可以在 ors 中查找
+//            ans[i] = ors.get(0)[1] - i + 1;
+        }
+        return ans;
     }
 }
