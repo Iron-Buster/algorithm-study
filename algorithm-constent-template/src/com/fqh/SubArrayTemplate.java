17cdd4c453cfe6173373fbd946744d603b3692a6
Iron-Buster <1457480465@qq.com>
2024-05-17 00:05:44 +0800

    🍗O(N)时间复杂度计算非空子数组和

--- algorithm-constent-template/src/com/fqh/SubArrayTemplate.java
+++ algorithm-constent-template/src/com/fqh/SubArrayTemplate.java
@@ -17,10 +17,11 @@ public class SubArrayTemplate {
      * O(N) 计算所有的非空子数组和
      * @param a
      */
-    public static long subArray(long[] a) {
+    public static long subArraySum(long[] a) {
         long sum = 0;
         int n = a.length;
         for (int i = 0; i < a.length; i++) {
+            // 累加每个a[i]的贡献
             sum += (a[i] * (i + 1) * (n - i));
         }
         return sum;
