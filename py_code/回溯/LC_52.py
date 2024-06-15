d7f2cd13122323447f2936461319ac7117594171
Iron-Buster <1457480465@qq.com>
2024-06-15 16:11:07 +0800

    Create LC_52.py

---
+++ py_code/回溯/LC_52.py
@@ -0,0 +1,39 @@
+# https://leetcode.cn/problems/n-queens-ii/
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        res = 0
+        board = [['.' for _ in range(n)] for _ in range(n)]
+        
+        def check(row: int, col: int) -> bool:
+            for i in range(row):
+                if board[i][col] == 'Q':
+                    return False
+            i = row - 1
+            j = col - 1
+            while i >= 0 and j >= 0:
+                if board[i][j] == 'Q':
+                    return False
+                i -= 1
+                j -= 1
+            i = row - 1
+            j = col + 1
+            while i >= 0 and j < n:
+                if board[i][j] == 'Q':
+                    return False
+                i -= 1
+                j += 1
+            return True
+        
+        def dfs(row: int) -> int:
+            nonlocal res
+            if row == n:
+                res += 1
+                return
+            for col in range(n):
+                if check(row, col):
+                    board[row][col] = 'Q'
+                    dfs(row + 1)
+                    board[row][col] = '.'
+        
+        dfs(0)
+        return res
