300af682d1cf22c27179c83c7e9b7caa3a47bbdf
Iron-Buster <1457480465@qq.com>
2024-07-25 15:20:31 +0800

    枚举末尾两位数字

---
+++ leetcode/src/com/fqh/枚举/LC_2844.java
@@ -0,0 +1,29 @@
+package com.fqh.枚举;
+
+public class LC_2844 {
+
+    public int minimumOperations(String num) {
+        // A number is divisible by 25 if its last two digits are 75, 50, 25, or 00.
+        int res1 = num.length() - (num.contains("0") ? 1 : 0); // 01这种case
+        int res2 = Math.min(
+                Math.min(f(num, 7, 5), f(num, 5, 0)),
+                Math.min(f(num, 2, 5), f(num, 0, 0)));
+        return Math.min(res1, res2);
+    }
+
+    public int f(String num, int x, int y) {
+        int ans = 0x3f3f3f;
+        for (int i = 0; i < num.length(); i++) {
+            if (num.charAt(i) - '0' == x) {
+                for (int j = i + 1; j < num.length(); j++) {
+                    if (num.charAt(j) - '0' == y) {
+                        int d1 = num.length() - j - 1;
+                        int d2 = j - i - 1;
+                        ans = Math.min(ans, d1 + d2);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
