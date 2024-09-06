736da9cb2557c45354f49f03f2ff4db25335e6b7
meilicat <1457480465@qq.com>
2024-09-06 20:55:14 +0800

    2024.9.5-XM-第2题-序列(开发岗)

--- codefun/src/com/fqh/xiaomi/P1993.java
+++ codefun/src/com/fqh/xiaomi/P1993.java
@@ -1,67 +1,48 @@
 package com.fqh.xiaomi;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
 public class P1993 {
 
+    // https://codefun2000.com/p/P1993
+    // 2024.9.5-XM-第2题-序列(开发岗)
 
-    // https://codefun2000.com/p/P1992
-    // 2024.9.5-XM-第1题-小塔吃面包(开发岗)
+    static int n, x;
+    static int[] a;
+    static int[][] dp = new int[1001][1001];
+    static final int INF = Integer.MAX_VALUE / 2;
 
 
-    static class PII {
-        int value;
-        int index;
-
-        public PII(int value, int index) {
-            this.value = value;
-            this.index = index;
+    // (a + b + c) % x == ((a % x) + (b % x) + (c % x))
+    static int dfs(int i, int v) {
+        if (i >= n) {
+            return v % x == 0 ? 0 : INF;
         }
+        if (dp[i][v] != -1) return dp[i][v];
+        // 不删除
+        int res = dfs(i + 1, (v + a[i]) % x);
+        // 删除 还是 +1
+        int op1 = dfs(i + 1, v) + 1;
+        int op2 = dfs(i + 1, (v + a[i] + 1) % x) + 1;
+        res = Math.min(res, Math.min(op1, op2));
+        return dp[i][v] = res;
     }
 
-
     public static void solve() throws IOException {
-        int n = in.nextInt();
-        int[] a = new int[n];
-        int[] b = new int[n];
+        n = in.nextInt();
+        x = in.nextInt();
+        a = new int[n];
         for (int i = 0; i < n; i++) a[i] = in.nextInt();
-        for (int i = 0; i < n; i++) b[i] = in.nextInt();
+        for (int[] d : dp) Arrays.fill(d, -1);
 
-        TreeSet<PII> tset = new TreeSet<>((x, y) -> x.value - y.value);
-        for (int i = 0; i < n; i++) {
-            tset.add(new PII(b[i], i));
-        }
-        long ans = Long.MAX_VALUE / 2;
-        for (int i = 0; i < n; i++) {
-            int c1 = a[i] + b[i];
-            PII ceiling = tset.ceiling(new PII(a[i], 0));
-            ans = Math.min(ans, c1);
-            if (ceiling == null) {
-                ans = Math.min(ans, a[i]);
-            } else if (ceiling.index != i) {
-                ans = Math.min(ans, ceiling.value);
-            }
-        }
-        tset.clear();
-        for (int i = 0; i < n; i++) {
-            tset.add(new PII(a[i], i));
-        }
-        for (int i = 0; i < n; i++) {
-            int c1 = a[i] + b[i];
-            PII ceiling = tset.ceiling(new PII(b[i], 0));
-            ans = Math.min(ans, c1);
-            if (ceiling == null) {
-                ans = Math.min(ans, b[i]);
-            } else if (ceiling.index != i) {
-                ans = Math.min(ans, ceiling.value);
-            }
-        }
-        out.println(ans);
+        int res = dfs(0, 0);
+        out.println(res);
     }
 
     static boolean MULTI_CASE = false;
+
     public static void main(String[] args) throws Exception {
         int T = MULTI_CASE ? in.nextInt() : 1;
         while (T-- > 0) {
@@ -72,6 +53,7 @@ public class P1993 {
 
     static InputReader in = new InputReader();
     static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
     static class InputReader {
         private StringTokenizer st;
         private BufferedReader bf;
