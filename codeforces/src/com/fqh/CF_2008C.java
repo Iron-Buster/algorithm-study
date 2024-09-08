6a39929e3abc239d76cb81ca3fd30ea7c33c7188
meilicat <1457480465@qq.com>
2024-09-08 17:01:04 +0800

    Codeforces Div3-970 C -> 暴力

---
+++ codeforces/src/com/fqh/CF_2008C.java
@@ -0,0 +1,70 @@
+package com.fqh;
+
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class CF_2008C {
+
+
+//    https://codeforces.com/contest/2008/problem/C
+
+    public static void solve() throws IOException {
+        int l = in.nextInt();
+        int r = in.nextInt();
+        int p = 1;
+        int ans = 0;
+        while (l <= r) {
+            l += p;
+            p++;
+            ans++;
+        }
+        out.println(ans);
+    }
+
+    static boolean MULTI_CASE = true;
+
+    public static void main(String[] args) throws Exception {
+        int T = MULTI_CASE ? in.nextInt() : 1;
+        while (T-- > 0) {
+            solve();
+        }
+        out.close();
+    }
+
+    static InputReader in = new InputReader();
+    static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+    static class InputReader {
+        private StringTokenizer st;
+        private BufferedReader bf;
+
+        public InputReader() {
+            bf = new BufferedReader(new InputStreamReader(System.in));
+            st = null;
+        }
+
+        public String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(bf.readLine());
+            }
+            return st.nextToken();
+        }
+
+        public String nextLine() throws IOException {
+            return bf.readLine();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+    }
+}
