3a03709ecec2c52decb89eb401e2429664c5594f
meilicat <1457480465@qq.com>
2024-09-25 23:41:10 +0800

    Codeforces div3-974 A 模拟

---
+++ codeforces/src/com/fqh/div3/ROUND_974/A.java
@@ -0,0 +1,74 @@
+package com.fqh.div3.ROUND_974;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class A {
+
+    public static void solve() throws IOException {
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = in.nextInt();
+        }
+        int sum = 0;
+        int ans = 0;
+        for (int x : a) {
+            if (x == 0) {
+                ans += sum > 0 ? 1 : 0;
+                sum = Math.max(0, sum - 1);
+            } else if (x >= k) {
+                sum += x;
+            }
+        }
+        out.println(ans);
+    }
+
+    static boolean MULTI_CASE = true;
+
+    public static void main(String[] args) throws Exception {
+        int T = MULTI_CASE ? in.nextInt() : 1;
+        while (T-- > 0) {
+            solve();
+        }
+        out.close();
+    }
+
+    static InputReader in = new InputReader();
+    static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+    static class InputReader {
+        private StringTokenizer st;
+        private BufferedReader bf;
+
+        public InputReader() {
+            bf = new BufferedReader(new InputStreamReader(System.in));
+            st = null;
+        }
+
+        public String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(bf.readLine());
+            }
+            return st.nextToken();
+        }
+
+        public String nextLine() throws IOException {
+            return bf.readLine();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+    }
+}
