65b4dc989b5297fc733f2790f026174f78756444
meilicat <1457480465@qq.com>
2024-10-05 15:31:55 +0800

    【update】 计算字符串任意s[i:j]是否是一个回文字符串

--- algorithm-constent-template/src/com/fqh/StringTemplate.java
+++ algorithm-constent-template/src/com/fqh/StringTemplate.java
@@ -181,3 +181,22 @@ class KMP {
         return ans;
     }
 }
+
+// 计算任意位置s[i:j]是否是一个回文字符串
+class PalString2D {
+
+    public boolean[][] calc(String s) {
+        int n = s.length();
+        boolean[][] ok = new boolean[n][n];
+        for (int i = 0; i < 2 * n - 1; i++) {
+            int l = i / 2;
+            int r = i / 2 + i % 2;
+            while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+                ok[l][r] = true;
+                l--;
+                r++;
+            }
+        }
+        return ok;
+    }
+}
